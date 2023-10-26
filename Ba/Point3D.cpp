fb521cb09cb9f32a6528843a775bc3fcf91df948
DeSTeR2 <defens213@gmail.com>
2023-10-26 23:42:58 +0300

    Add some func to Point3D class

--- Ba/Point3D.cpp
+++ Ba/Point3D.cpp
@@ -1,6 +1,20 @@
 #include "Point3D.h"
-Point3D::Point3D(float x, float y, float z) {
+Point3D::Point3D(float x, float y, float z,int num) {
 	this->x = x;
 	this->y = y;
 	this->z = z;
+	this->num = num;
+}
+
+float Point3D::getX() {
+	return this->x;
+}
+float Point3D::getY() {
+	return this->y;
+}
+float Point3D::getZ() {
+	return this->z;
+}
+int Point3D::getNum() {
+	return this->num;
 }
--- Ba/Point3D.h
+++ Ba/Point3D.h
@@ -3,8 +3,14 @@ class Point3D
 {
 private:
 	float x, y, z;
+	int num;
 public: 
 	Point3D();
-	Point3D(float x, float y, float z);
+	Point3D(float x, float y, float z, int num);
+
+	float getX();
+	float getY();
+	float getZ();
+	int getNum();
 };
 
